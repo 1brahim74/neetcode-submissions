fe06b24862c7db04df9ff9bc19b5034bf09f5cd7
Ibrahim Mammadov <128031580+1brahim74@users.noreply.github.com>
2026-08-29 14:15:41 +0800

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int stoi(string inp) {
+        if (inp.empty()) return 0;
+        
+        long long ans = 0; 
+        bool isNegative = false;
+        int startIndex = 0;
+        if (inp[0] == '-') {
+            isNegative = true;
+            startIndex = 1;
+        }
+        for (int i = startIndex; i < inp.size(); i++) {
+            int num = inp[i] - '0'; 
+            ans = ans * 10 + num;
+        }
+        
+        if (isNegative) {
+            return -ans;
+        }
+        return ans;
+    }
+    string itos(int inp) {
+        
+        if (inp == 0) return "0";
+        
+        string ans = "";
+        bool isNegative = false;
+        long long num = inp; 
+        if (num < 0) {
+            isNegative = true;
+            num = -num; 
+        }
+
+        while (num > 0) {
+            char digit = (num % 10) + '0'; 
+            ans = ans + digit;
+            num = num / 10;
+        }
+        
+        if (isNegative) {
+            ans = ans + '-';
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+        
+        for (int i = 0; i < tokens.size(); i++) {
+            string token = tokens[i];
+            if (token == "+" || token == "-" || token == "*" || token == "/") {
+                int val2 = s.top(); 
+                s.pop();
+                int val1 = s.top(); 
+                s.pop();
+                
+                if (token == "+") s.push(val1 + val2);
+                else if (token == "-") s.push(val1 - val2);
+                else if (token == "*") s.push(val1 * val2);
+                else if (token == "/") s.push(val1 / val2);
+            } 
+            else {
+                s.push(stoi(token)); 
+            }
+        }
+        
+        
+        return s.top();
+    }
+
+};
