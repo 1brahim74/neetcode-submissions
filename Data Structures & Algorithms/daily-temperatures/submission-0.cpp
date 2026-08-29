59101e1ed2ec10dcaad42c12d469265955c891c3
Ibrahim Mammadov <128031580+1brahim74@users.noreply.github.com>
2026-08-29 15:34:12 +0800

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int>s;int n=temperatures.size();
+        s.push(0);
+        vector<int>res(n,0);
+        for(int i=1;i<n;i++)
+        {
+            //cout<<temperatures[s.top()]<<" "<<temperatures[i]<<endl;
+            if(temperatures[s.top()]<temperatures[i])
+            
+                while(temperatures[s.top()]<temperatures[i])
+                {
+                    //cout<<"warmer\n";
+                    res[s.top()]=i-s.top();
+                    s.pop();
+                    if(s.empty())
+                        break;
+                }
+            else
+                s.push(i);
+            s.push(i);
+        }
+        return res;
+    }
+};
